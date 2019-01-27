b1ed83a82ab60433c5163cf825f18e1ab042f31c
andy.chen <405643529@qq.com>
2019-01-27 17:00:54 +0800

    第一次提交

--- Startup.cs
+++ Startup.cs
@@ -57,6 +57,7 @@ namespace AspNetCoreTodo
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //这里是强制使用Https
                 app.UseHsts();
             }
 
